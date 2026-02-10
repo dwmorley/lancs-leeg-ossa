8ba4e2ec41688118e01bf7ef633fc95d59ff04d0
David Morley <d.morley1@lancaster.ac.uk>
2026-02-10 16:39:47 +0000

    backup

--- src/sampling/asd.py
+++ src/sampling/asd.py
@@ -3,6 +3,10 @@ from rpy2.robjects.conversion import localconverter
 from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
 import rpy2.robjects as ro
+import numpy as np
+import xarray as xr
+import rioxarray  # noqa: F401
+import matplotlib.pyplot as plt
 
 
 def glmmPQL_via_rpy2(formulaf, formular, data, area, target, total, delta):
@@ -80,6 +84,7 @@ def glmmPQL_via_rpy2(formulaf, formular, data, area, target, total, delta):
 
         if target == "U":
             # TODO:
+
             colnames = """colnames(x)=c("x","y","Uncertainty")"""
 
     ro.r("""xx <- dist(x[, 1:2])""")
@@ -89,16 +94,59 @@ def glmmPQL_via_rpy2(formulaf, formular, data, area, target, total, delta):
     )
     ro.r("""xx <- unique(unlist(xx))""")
     ro.r("""x <- x[-xx,]""")
-    ro.r("""x <- x[1:{total},]""")
+    ro.r(f"""x <- x[1:{total},]""")
     ro.r(colnames)
+
+    ro.r(
+        """
+        image <- setNames(
+            data.frame(modelgridX$x, modelgridX$y, modelgridX$z),
+            c("x", "y", "z")
+        )
+    """
+    )
 
     x_df = ro.globalenv["x"]
     with localconverter(pandas2ri.converter):
         x_df = pandas2ri.rpy2py(x_df)
 
-    # TODO: return DF and plot image
+    x_array = np.array(ro.r("modelgridX$x")).flatten()
+    y_array = np.array(ro.r("modelgridX$y")).flatten()
+    z_array = np.array(ro.r("modelgridX$z"))
+    z_grid = z_array.reshape(len(x_array), len(y_array)).T
 
-    return x_df
+    da = xr.DataArray(z_grid, coords={"y": y_array, "x": x_array}, dims=["y", "x"])
+
+    # # Assign CRS and save
+    da.rio.write_crs("EPSG:4326", inplace=True)
+    da.rio.to_raster("adaptive_sampling.tif")  # VERIFIED THE SAME
+
+    # TODO: TUES - overlay the 15 points
+    # TODO: TUES - for U as well
+
+    from matplotlib.colors import BoundaryNorm
+    from matplotlib.cm import get_cmap
+
+    vmin, vmax = z_grid.min(), z_grid.max()
+    bounds = np.linspace(vmin, vmax, 12)
+    norm = BoundaryNorm(bounds, get_cmap("viridis").N)
+    fig, ax = plt.subplots(figsize=(10, 8))
+    im = ax.imshow(
+        z_grid,
+        extent=(x_array.min(), x_array.max(), y_array.min(), y_array.max()),
+        origin="lower",
+        cmap="hot",
+        norm=norm,
+    )
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_title("Adaptive Sampling Raster (EPSG:4326)")
+    cbar = plt.colorbar(im, ax=ax, boundaries=bounds, ticks=bounds)
+    cbar.set_label("Value")
+    plt.tight_layout()
+    plt.show()
+
+    return x_df  # Output is raster and points
 
 
 if __name__ == "__main__":
